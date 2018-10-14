a6323e2c0ee7739ba1b4398d3aeb1d7b1800d4ce
Johnny Silva <johnnysilva_estagio@hotmail.com>
2018-10-14 13:35:32 -0300

    populando o banco de dados

--- src/br/com/caelum/financas/teste/TesteConta.java
+++ src/br/com/caelum/financas/teste/TesteConta.java
@@ -5,6 +5,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 import br.com.caelum.financas.modelo.Conta;
+import br.com.caelum.financas.util.JPAUtil;
 
 public class TesteConta {
 	
@@ -17,8 +18,7 @@ public class TesteConta {
         conta.setAgencia("123");
         conta.setNumero("456");
 		
-        EntityManagerFactory emf =  Persistence.createEntityManagerFactory("financas");
-        EntityManager em = emf.createEntityManager();
+        EntityManager em = new JPAUtil().getEntityManager();
 
 
         em.getTransaction().begin();
@@ -26,7 +26,6 @@ public class TesteConta {
         em.getTransaction().commit();
 
         em.close();
-        emf.close();
 		
 
 
